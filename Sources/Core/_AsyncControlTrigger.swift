1598eb0fc741d239b4830b4615ad54e683acee0d
treastrain / Tanaka Ryoga <tr@tret.jp>
2023-11-05 02:22:57 +0900

    Support AsyncControlView action argument values

--- Sources/Core/_AsyncControlTrigger.swift
+++ Sources/Core/_AsyncControlTrigger.swift
@@ -9,15 +9,19 @@ import Foundation
 
 /// - Warning: This `Equatable` compliance is a dummy implementation added for use in `SwiftUI.View.onPreferenceChange(_:perform:)`.
 @available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
-package struct _AsyncControlTrigger: Equatable {
-    private let action: () -> Void
+package struct _AsyncControlTrigger<Value>: Equatable {
+    private let action: (_ value: Value) -> Void
     
-    init(action: @escaping () -> Void) {
+    init(action: @escaping (_ value: Value) -> Void) {
         self.action = action
     }
     
-    package func callAsFunction() {
-        action()
+    package func callAsFunction(_ value: Value) {
+        action(value)
+    }
+    
+    package func callAsFunction() where Value == Void {
+        callAsFunction(())
     }
     
     package static func == (lhs: Self, rhs: Self) -> Bool {
--- Sources/Core/_AsyncControlTriggerPreferenceKey.swift
+++ Sources/Core/_AsyncControlTriggerPreferenceKey.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @available(iOS 15.0, macOS 12.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
-package struct _AsyncControlTriggerPreferenceKey: PreferenceKey {
-    package static let defaultValue = _AsyncControlTrigger(action: {})
-    package static func reduce(value: inout _AsyncControlTrigger, nextValue: () -> _AsyncControlTrigger) {}
+package struct _AsyncControlTriggerPreferenceKey<Value>: PreferenceKey {
+    package static var defaultValue: _AsyncControlTrigger<Value> { fatalError("This property is not expected to be called.") }
+    package static func reduce(value: inout _AsyncControlTrigger<Value>, nextValue: () -> _AsyncControlTrigger<Value>) { fatalError("This function is not expected to be called.") }
 }
